645cf7dc2ae263097c663c154e7167ce41aaa6c6
yijiliu <yijiliu@paypal.com>
2015-04-28 12:59:18 +0800

    local change

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -11,8 +11,8 @@
 #
 
 HISTFILE="${ZDOTDIR:-$HOME}/.zsh_history"       # The path to the history file.
-HISTSIZE=10000                   # The maximum number of events to save in the internal history.
-SAVEHIST=10000                   # The maximum number of events to save in the history file.
+HISTSIZE=16448                   # The maximum number of events to save in the internal history.
+SAVEHIST=16448                   # The maximum number of events to save in the history file.
 
 #
 # Options
